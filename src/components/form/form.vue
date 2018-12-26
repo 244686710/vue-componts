ccb08cdb345ffa7f913580c31f5e851b13f96110
yu.yadong <yuyadong@zgrj.net>
2018-12-26 11:06:21 +0800

    组件之间数据传递provide inject使用

--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -1,18 +1,37 @@
 <template>
-    <form>
-        <slot></slot>
-    </form>
+  <form>
+    <slot></slot>
+  </form>
 </template>
 <script>
-export default {
+  import Emitter from '../../mixins/emitter'
+  export default {
     name: 'iForm',
+    mixins: [Emitter],
     props: {
-        model: {
-            type: Object
-        },
-        rules: {
-            type: Object
-        }
+      model: {
+        type: Object
+      },
+      rules: {
+        type: Object
+      }
+    },
+    // 要拿到 Form 中 model 里的数据，
+    // provide / inject。所以在 Form 中，
+    // 把整个实例（this）向下提供，并在 FormItem 中注入：
+    provide() {
+      return {
+        form: this
+      }
+    },
+    created() {
+      this.$on('on-form-item-add', (field) => {
+        if (field) this.fields.push(field);
+      });
+      this.$on('on-form-item-remove', (field) => {
+        if (field.prop) this.fields.splice(this.fields.indexOf(field), 1);
+      });
     }
-}
+  }
+
 </script>
--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,10 +1,45 @@
 <template>
-    <div>
-        <input type="text">
-    </div>
+  <div>
+    <input type="text" :value="currentValue" @input="handleInput" @blur="handleBlur" />
+  </div>
 </template>
 <script>
-export default {
-    name: 'iInput'
-}
+  import Emitter from '../../mixins/emitter.js'
+  export default {
+    name: 'iInput',
+    mixins: [Emitter],
+    props: {
+      value: {
+        type: String,
+        default: ''
+      }
+    },
+    data () {
+      return {
+        currentValue: this.value
+      }
+    },
+
+    watch: {
+      value (val) {
+        this.currentValue = val;
+      }
+    },
+
+    methods: {
+      handleInput (event) {
+        const value = event.target.value;
+        this.currentValue = value;
+        this.$emit('input', value);
+        this.dispatch('iFromItem', 'on-form-change', value);
+      },
+
+      handleBlur () {
+        this.dispatch('iFormItem', 'on-form-blur', this.currentValue);
+      }
+    }
+
+
+  }
+
 </script>
